10f6d6d4063109e2736f87e5f2c04d05308da64d
minami <sourcechord@gmail.com>
2017-09-13 00:32:10 +0900

    Initial Commit

---
+++ view/canvas-datagrid/index.js
@@ -0,0 +1,7 @@
+var grid = canvasDatagrid({
+    parentNode: document.getElementById('grid'),
+    data: [
+        {col1: 'row 1 column 1', col2: 'row 1 column 2', col3: 'row 1 column 3'},
+        {col1: 'row 2 column 1', col2: 'row 2 column 2', col3: 'row 2 column 3'}
+    ]
+});
